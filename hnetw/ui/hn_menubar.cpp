f7a0091cce2a0a06327571926b30b5573e52cf52
Dzmitry Haron <pitbull9846@gmail.com>
2024-01-02 19:46:10 +0300

    Ui: Remove File submenu

--- hnetw/ui/hn_menubar.cpp
+++ hnetw/ui/hn_menubar.cpp
@@ -5,7 +5,6 @@
 HnMenuBar::HnMenuBar(int width, QWidget* parent) : QMenuBar(parent), interfacesIpStrings_()
 {
     setGeometry(QRect(0, 0, width, 22));
-    createFileSubmenu();
     createCaptureSubmenu();
 }
 
@@ -21,18 +20,6 @@ void HnMenuBar::setInterfacesIp(std::vector<std::string> ipStrings)
 void HnMenuBar::showChangeInterfaceDialog()
 {
     handleInterfaceChange();
-}
-
-void HnMenuBar::createFileSubmenu() {
-    addSubmenu(&menuFile_, "File");
-    addSubmenuAction(this, menuFile_, &fileOpenAction_, "openAction",
-                     "Open", "Ctrl+O");
-    fileOpenAction_->connect(fileOpenAction_, &QAction::triggered,
-                             this, &HnMenuBar::handleOpenFile);
-    addSubmenuAction(this, menuFile_, &fileSaveAction_, "saveAction",
-                     "Save", "Ctrl+S");
-    fileSaveAction_->connect(fileSaveAction_, &QAction::triggered,
-                             this, &HnMenuBar::handleSaveFile);
 }
 
 void HnMenuBar::createCaptureSubmenu() 
@@ -58,43 +45,6 @@ void HnMenuBar::addSubmenuAction(QWidget* parent, QMenu* submenu, QAction** acti
     submenu->addAction(*action);
     (*action)->setText(title);
     (*action)->setShortcut(QKeySequence(shortcut));
-}
-
-void HnMenuBar::handleOpenFile()
-{
-    //QString fileName = QFileDialog::getOpenFileName(this, nullptr, nullptr, "Simple Paint File (*.smp)");
-    //QFile file(fileName);
-    //QDataStream stream(&file);
-    //file.open(QIODevice::ReadOnly);
-
-    //if (file.size() == 0) {
-    //    file.close();
-    //    return;
-    //}
-
-    //QVector<Shape*>& shapesList = SmpDrawer::getInstance()->getShapesList();
-    //if (shapesList.size() > 0) { SmpDrawer::getInstance()->removeDrawnShapes(); }
-
-    //deserializeShapesList(stream, shapesList);
-    //file.close();
-}
-
-void HnMenuBar::handleSaveFile()
-{
-    //QString fileName = QFileDialog::getSaveFileName(this, nullptr, "Untitled.smp", "Simple Paint File (*.smp)");
-    //QFile file(fileName);
-    //QDataStream stream(&file);
-    //file.open(QIODevice::WriteOnly);
-
-    //QVector<Shape*>& shapesList = SmpDrawer::getInstance()->getShapesList();
-
-    //if (shapesList.isEmpty()) {
-    //    file.close();
-    //    return;
-    //}
-
-    //serializeShapesList(stream, shapesList);
-    //file.close();
 }
 
 void HnMenuBar::handleInterfaceChange()
